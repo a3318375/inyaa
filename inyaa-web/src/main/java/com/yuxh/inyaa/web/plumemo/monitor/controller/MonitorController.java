45c74a6fdb9890e092405a307e7622a50c068bb0
yxhao <184608371@qq.com>
2021-02-26 17:31:55 +0800

    1

--- inyaa-web/src/main/java/com/yuxh/inyaa/web/plumemo/monitor/controller/MonitorController.java
+++ inyaa-web/src/main/java/com/yuxh/inyaa/web/plumemo/monitor/controller/MonitorController.java
@@ -1,12 +1,12 @@
 package com.yuxh.inyaa.web.plumemo.monitor.controller;
 
-import com.byteblogs.common.annotation.LoginRequired;
-import com.byteblogs.common.base.domain.Result;
-import com.byteblogs.system.enums.RoleEnum;
+import com.yuxh.inyaa.web.common.annotation.LoginRequired;
+import com.yuxh.inyaa.web.common.base.domain.Result;
+import com.yuxh.inyaa.web.plumemo.monitor.util.RuntimeUtil;
+import com.yuxh.inyaa.web.system.enums.RoleEnum;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.byteblogs.plumemo.monitor.util.RuntimeUtil;
 
 @RestController
 @RequestMapping("/monitor")
@@ -22,5 +22,34 @@ public class MonitorController {
     @LoginRequired(role = RoleEnum.ADMIN)
     public Result getMemory(){
         return RuntimeUtil.getMemory();
+    }
+
+    @GetMapping("/cpu/v1/get")
+    public Result getCpu() {
+        return RuntimeUtil.getCpu();
+    }
+
+    @GetMapping("/file/v1/get")
+    public Result getFile() {
+        return RuntimeUtil.getFile();
+    }
+
+    @GetMapping("/net/v1/get")
+    public Result getNet() {
+        return RuntimeUtil.getNet();
+    }
+
+    @GetMapping("/ethernet/v1/get")
+    public Result getEthernet() {
+        return RuntimeUtil.getEthernet();
+    }
+
+    @GetMapping("/user/v1/count")
+    public Result getUserCount() {
+        return RuntimeUtil.getUserCount();
+    }
+    @GetMapping("/user/v1/chart")
+    public Result getUserChat() {
+        return RuntimeUtil.getUserChat();
     }
 }
