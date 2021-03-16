c400f5f493fca9b9902bd591005da0cd3290b03e
yxhao <184608371@qq.com>
2021-03-16 15:14:10 +0800

    1

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
 FROM openjdk:8-jdk-alpine
-MAINTAINER 184608371@qq.com
 
-#对外暴漏的端口号
-EXPOSE 8082
-WORKDIR /
+VOLUME /root/tmp
 
 #将本地项目jar包拷贝到Docker容器中的位置
-RUN cp ./inyaa-web/build/libs/inyaa-web.jar ./
+ADD inyaa-web/build/libs/inyaa-web.jar inyaa-web.jar
 
 #开机启动
 ENTRYPOINT ["java","-jar","/inyaa-web.jar"]
---
+++ inyaa-security/src/main/java/com/inyaa/security/InyaaSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.inyaa.security;
+
+import com.inyaa.base.InyaaBaseApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyaaSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InyaaBaseApplication.class, args);
+    }
+
+}
