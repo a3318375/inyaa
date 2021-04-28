f9240050c8b58c1e50fbe21c29565081ce915aef
yuxh <184608371@qq.com>
2021-04-28 08:11:46 +0800

    评论功能3

--- inyaa-web/src/main/java/com/inyaa/web/config/WebSecurityConfig.java
+++ inyaa-web/src/main/java/com/inyaa/web/config/WebSecurityConfig.java
@@ -6,13 +6,18 @@ import com.inyaa.web.auth.dao.SysApiDao;
 import com.inyaa.web.auth.service.OauthUserService;
 import com.inyaa.web.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.access.event.LoggerListener;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
@@ -24,14 +29,27 @@ import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientServ
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <h1>WebSecurityConfig</h1>
@@ -76,8 +94,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         });
 
 
-        http.oauth2Login().defaultSuccessUrl("https://oauth.inyaa.cn")
-                .userInfoEndpoint().userService(oauthUserService);
+        http.oauth2Login(oauth2LoginConfigurer -> oauth2LoginConfigurer
+                .tokenEndpoint(tokenEndpointConfig -> tokenEndpointConfig.accessTokenResponseClient(oAuth2AccessTokenResponseClient()))
+                .defaultSuccessUrl("https://oauth.inyaa.cn")
+                .userInfoEndpoint().userService(oauthUserService)
+        );
+
         http.oauth2Client();
         http.logout().logoutSuccessHandler((req, resp, authentication) -> {
             BaseResult<String> res = BaseResult.success();
@@ -137,6 +159,67 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public OAuth2AuthorizedClientService oAuth2AuthorizedClientService(JdbcOperations jdbcOperations, ClientRegistrationRepository clientRegistrationRepository) {
         return new JdbcOAuth2AuthorizedClientService(jdbcOperations, clientRegistrationRepository);
     }
+
+    /**
+     * qq获取access_token返回的结果是类似get请求参数的字符串，无法通过指定Accept请求头来使qq返回特定的响应类型，并且qq返回的access_token
+     * 也缺少了必须的token_type字段（不符合oauth2标准的授权码认证流程），spring-security默认远程获取
+     * access_token的客户端是{@link DefaultAuthorizationCodeTokenResponseClient}，所以我们需要
+     * 自定义{@link QqoAuth2AccessTokenResponseHttpMessageConverter}注入到这个client中来解析qq的access_token响应信息
+     *
+     * @return {@link DefaultAuthorizationCodeTokenResponseClient} 用来获取access_token的客户端
+     * @see <a href="https://www.oauth.com/oauth2-servers/access-tokens/authorization-code-request">authorization-code-request规范</a>
+     * @see <a href="https://www.oauth.com/oauth2-servers/access-tokens/access-token-response">access-token-response规范</a>
+     * @see <a href="https://wiki.connect.qq.com/%E5%BC%80%E5%8F%91%E6%94%BB%E7%95%A5_server-side">qq开发文档</a>
+     */
+    private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient() {
+        DefaultAuthorizationCodeTokenResponseClient client = new DefaultAuthorizationCodeTokenResponseClient();
+        RestTemplate restTemplate = new RestTemplate(Arrays.asList(
+                new FormHttpMessageConverter(),
+
+                // 解析标准的AccessToken响应信息转换器
+                new OAuth2AccessTokenResponseHttpMessageConverter(),
+
+                // 解析qq的AccessToken响应信息转换器
+                new QqoAuth2AccessTokenResponseHttpMessageConverter(MediaType.TEXT_HTML)));
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        client.setRestOperations(restTemplate);
+        return client;
+    }
+
+    /**
+     * 自定义消息转换器来解析qq的access_token响应信息
+     *
+     * @see OAuth2AccessTokenResponseHttpMessageConverter#readInternal(java.lang.Class, org.springframework.http.HttpInputMessage)
+     * @see OAuth2LoginAuthenticationProvider#authenticate(org.springframework.security.core.Authentication)
+     */
+    private static class QqoAuth2AccessTokenResponseHttpMessageConverter extends OAuth2AccessTokenResponseHttpMessageConverter {
+
+        public QqoAuth2AccessTokenResponseHttpMessageConverter(MediaType... mediaType) {
+            setSupportedMediaTypes(Arrays.asList(mediaType));
+        }
+
+        @SneakyThrows
+        @Override
+        protected OAuth2AccessTokenResponse readInternal(Class<? extends OAuth2AccessTokenResponse> clazz, HttpInputMessage inputMessage) {
+
+            String response = StreamUtils.copyToString(inputMessage.getBody(), StandardCharsets.UTF_8);
+
+            log.info("qq的AccessToken响应信息：{}", response);
+
+            // 解析响应信息类似access_token=YOUR_ACCESS_TOKEN&expires_in=3600这样的字符串
+            Map<String, String> tokenResponseParameters = Arrays.stream(response.split("&")).collect(Collectors.toMap(s -> s.split("=")[0], s -> s.split("=")[1]));
+
+            // 手动给qq的access_token响应信息添加token_type字段，spring-security会按照oauth2规范校验返回参数
+            tokenResponseParameters.put(OAuth2ParameterNames.TOKEN_TYPE, "bearer");
+            return this.tokenResponseConverter.convert(tokenResponseParameters);
+        }
+
+        @Override
+        protected void writeInternal(OAuth2AccessTokenResponse tokenResponse, HttpOutputMessage outputMessage) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
 
     /**
      * 认证事件监听器，打印日志
